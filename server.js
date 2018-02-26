8cad5e03976f28058b6a9899184b47985f8c752c
Glitch (searchimages) <none>
2018-02-26 13:22:52 +0000

    ☃️📅 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const express = require("express");
+
+const app = express();
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  const { method, url } = req;
+  console.log({ method, url });
+  next();
+});
+app.use(express.static("public"));
+
+const GoogleImages = require("google-images");
+const client = new GoogleImages(process.env.CSE_ID, process.env.API_KEY);
+
+const termArr = [];
+app.get("/recent", (req, res) => {
+  res.json(termArr);
+});
+
+app.get("/search/:term", (req, res) => {
+  const { term } = req.params;
+  const pageNum = +req.query.offset;
+  console.log({ term, pageNum });
+  const dt = new Date();
+  termArr.unshift({ "Search term": term, "Time of search": dt.toString() });
+  if (termArr.length > 10) {
+    termArr.pop();
+  }
+  client
+    .search(term, { page: (pageNum - 1) * 10 + 1 })
+    .then(images => {
+      const cleanArr = images.map(({ url, description, parentPage }) => ({
+        url,
+        description,
+        parentPage
+      }));
+      res.json(cleanArr);
+    })
+    .catch(error => {
+      console.error(error);
+      res.send(error.message);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Node.js listening on ${port}...`);
+});
